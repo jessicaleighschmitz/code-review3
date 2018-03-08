c0cd6582d08319397730b0461ea5e1b38b7d0349
Guest User <Guest@Epicodus-8A.local>
2018-03-07 20:56:06 -0800

    add UI and Business Logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,31 @@
-$(function(){
-  $('#main-content form').submit(function(event) {
-    var numInput = parseInt($('input#userInput').val());
-    event.preventDefault();
-    $('#sub-content').show();
 
-    $('#output').text(numInput);
+//var outputArr =[];
+var ul = $('#output ul');
+function countUp(numInput) {
+  const outputArr =[];
+  for (var i = 1; i <= numInput; i++) {
+    if (i % 15 === 0) {
+      outputArr.push("Ping-Pong");
+    } else if (i % 5 === 0) {
+      outputArr.push("pong");
+    } else if (i % 3 === 0) {
+      outputArr.push("Ping");
+    } else {
+      outputArr.push(i);
+    };
+
+  };
+  return outputArr;
+};
+
+
+$(document).ready(function() {
+  $("#main-content form").submit(function(event) {
+    event.preventDefault();
+    var numInput = parseInt($("input#userInput").val());
+    var result = countUp(numInput);
+
+    for ()
+    $("#output").text(result);
   });
 });
